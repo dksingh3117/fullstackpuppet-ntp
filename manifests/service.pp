17cdb9391317d53b4372fdb81aa8b531f881a56d
Deepak Singh <dksingh3117@gmail.com>
2017-06-05 06:53:17 +0000

    Initial commit of fullstackpuppet-ntp

---
+++ manifests/service.pp
@@ -0,0 +1,13 @@
+# == Class: ntp::service
+class ntp::service inherits ntp {
+
+  service { 'ntp':
+    ensure     => running,
+    enable     => true,
+    hasstatus  => true,
+    hasrestart => true,
+    require => Package['ntp'],
+  }
+
+}
+
